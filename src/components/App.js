de5ceac456b7276dd3ca46ce93b84d42237cfff1
Weslley <weslley@pop-os.localdomain>
2022-09-30 17:45:11 -0300

    feat(exibicao-fotos): fim da pagina 27 da apostila 07

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import { Card } from 'primereact/card';
 import Busca from './Busca';
-import React from 'react'
-import env from 'react-dotenv'
+import React from 'react';
+import env from 'react-dotenv';
+import {createClient} from 'pexels';
 
 export default class App extends React.Component{
 
+    pexelsClient = null
+    state = {
+        pics:[]
+    }
+
     onBuscaRealizada = (termo) =>{
         console.log(termo)
+        this.pexelsClient.photos.search({query: termo})
+        .then(res => {
+            this.setState({pics: res.photos})}
+        )
     }
+
     
+    componentDidMount(){
+        this.pexelsClient = createClient(env.PEXELS_KEY)
+    }
+
 
     render(){
         console.log(env.PEXERLS_KEY);
@@ -20,10 +35,19 @@ export default class App extends React.Component{
                 <div className="col-12">
                 <h1>Exibir uma lista de...</h1>
                 </div>
-                <div className="col-8">          
+                <div className="col-12 md:col-8">          
                     <Card>
                         <Busca onBuscaRealizada={this.onBuscaRealizada}/>
                     </Card>      
+                </div>
+                <div className="col-12 md:col-8">
+                    {
+                        this.state.pics.map((pic, key)=>(
+                            <div key={pic.id}>
+                                <img src={pic.src.small} alt={pic.alt}/>
+                            </div>    
+                        ))
+                    }
                 </div>
             </div>  
         )
